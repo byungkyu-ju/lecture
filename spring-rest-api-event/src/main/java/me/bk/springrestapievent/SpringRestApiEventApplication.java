ca0ddb14cd9f31cc54e799b0d961d035158253d7
byungkyu-ju <noel1227@gmail.com>
2019-11-11 13:40:37 +0900

    feat (init spring-rest-api-event)

---
+++ spring-rest-api-event/src/main/java/me/bk/springrestapievent/SpringRestApiEventApplication.java
@@ -0,0 +1,13 @@
+package me.bk.springrestapievent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiEventApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestApiEventApplication.class, args);
+    }
+
+}
