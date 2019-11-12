f548ded253368973ce10733933bd9fd03a759f34
byungkyu-ju <noel1227@gmail.com>
2019-11-12 12:57:29 +0900

    feat (입력값 제한)

--- spring-rest-api-event/src/test/java/me/bk/springrestapievent/events/EventControllerTest.java
+++ spring-rest-api-event/src/test/java/me/bk/springrestapievent/events/EventControllerTest.java
@@ -32,6 +32,33 @@ class EventControllerTest {
     @Test
     @DisplayName("입력값 전달시 JSON 응답으로 201이 나오는지 확인")
     void createEvent() throws Exception {
+        EventDto event = EventDto.builder()
+                .name("Spring")
+                .description("description")
+                .beginEnrollmentDateTime(LocalDateTime.of(2019,11,11,9, 0))
+                .closeEnrollmentDateTime(LocalDateTime.of(2019,11,11,18,0))
+                .beginEventDateTime(LocalDateTime.of(2019,11,12,9,0))
+                .endEventDateTime(LocalDateTime.of(2019,11,12,18,0))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남")
+                .build();
+
+        mockMvc.perform(post("/api/events/")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaTypes.HAL_JSON)
+                    .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists())
+                .andExpect(header().exists(HttpHeaders.LOCATION))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE));
+    }
+
+    @Test
+    @DisplayName("id같은 항목은 user로 부터 요청받으면 안되기 때문에 bad case로 변경함")
+    void createEvent_bad_reqeust() throws Exception {
         Event event = Event.builder()
                 .id(100)
                 .name("Spring")
@@ -49,13 +76,10 @@ class EventControllerTest {
                 .build();
 
         mockMvc.perform(post("/api/events/")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .accept(MediaTypes.HAL_JSON)
-                    .content(objectMapper.writeValueAsString(event)))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(event)))
                 .andDo(print())
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("id").exists())
-                .andExpect(header().exists(HttpHeaders.LOCATION))
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE));
+                .andExpect(status().isBadRequest());
     }
 }
