78c0801ba19614e67acedaad5c714bad7e3f6473
byungkyu-ju <noel1227@gmail.com>
2020-05-27 15:39:36 +0900

    feat: 값타입 테스트

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -15,8 +16,20 @@ public class JpaMain {
             Member member = new Member();
             member.setName("name");
             member.setHomeAddress(new Address("city", "street", "zipcode"));
+            member.getFavoriteFoods().add("chicken");
+            member.getFavoriteFoods().add("pizza");
+            member.getAddressHistory().add(new Address("oldCity1", "street", "zipcode"));
+            member.getAddressHistory().add(new Address("oldCity2", "street", "zipcode"));
 
             entityManager.persist(member);
+
+            entityManager.flush();
+            entityManager.clear();
+            Member findMember =entityManager.find(Member.class, member.getId());
+            List<Address> addressHistory = findMember.getAddressHistory();
+            for(Address address : addressHistory){
+                System.out.println("address = " + address.getCity());
+            }
 
             entityTransaction.commit();
         } catch (Exception e) {
