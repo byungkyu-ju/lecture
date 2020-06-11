ab7c7f53f38384abb2f2863215bb6ddb22bd7c6d
byungkyu-ju <noel1227@gmail.com>
2020-06-12 00:05:59 +0900

    feat: stock up/down 로직 추가

--- jpastore/src/main/java/jpabook/jpastore/domain/item/Item.java
+++ jpastore/src/main/java/jpabook/jpastore/domain/item/Item.java
@@ -1,5 +1,6 @@
 package jpabook.jpastore.domain.item;
 
+import jpabook.jpastore.domain.exception.NotEnoughStockException.NotEnoughStockException;
 import lombok.Generated;
 import lombok.Getter;
 import lombok.Setter;
@@ -26,4 +27,22 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    /*
+        stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /*
+        stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
