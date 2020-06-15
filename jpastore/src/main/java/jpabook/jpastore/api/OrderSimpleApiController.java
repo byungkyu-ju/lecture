1659f5095ad8d1485f4c2ca8dd2023b66cf152d8
byungkyu-ju <noel1227@gmail.com>
2020-06-15 20:07:07 +0900

    feat: 조회용 샘플

---
+++ jpastore/src/main/java/jpabook/jpastore/api/OrderSimpleApiController.java
@@ -0,0 +1,24 @@
+package jpabook.jpastore.api;
+
+import jpabook.jpastore.domain.Order;
+import jpabook.jpastore.repository.OrderRepository;
+import jpabook.jpastore.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
+        return all;
+    }
+
+}
