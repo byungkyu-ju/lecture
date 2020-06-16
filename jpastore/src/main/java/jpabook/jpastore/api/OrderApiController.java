032d7149147a817cd481c3f51641f263494c64b2
byungkyu-ju <noel1227@gmail.com>
2020-06-16 11:04:50 +0900

    feat: 컬렉션조회 최적화 테스트를 위한 엔티티조회 직접 노출 케이스

---
+++ jpastore/src/main/java/jpabook/jpastore/api/OrderApiController.java
@@ -0,0 +1,29 @@
+package jpabook.jpastore.api;
+
+import jpabook.jpastore.domain.Order;
+import jpabook.jpastore.domain.OrderItem;
+import jpabook.jpastore.repository.OrderRepository;
+import jpabook.jpastore.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o -> o.getItem().getName());
+        }
+        return all;
+    }
+}
