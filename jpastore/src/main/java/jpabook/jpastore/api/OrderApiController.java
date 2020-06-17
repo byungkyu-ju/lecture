a720cec10086dd4bd4c91c428af641dc40c42c6b
byungkyu-ju <noel1227@gmail.com>
2020-06-17 14:11:18 +0900

    feat: Collection 조회 최적화

--- jpastore/src/main/java/jpabook/jpastore/api/OrderApiController.java
+++ jpastore/src/main/java/jpabook/jpastore/api/OrderApiController.java
@@ -72,6 +72,11 @@ public class OrderApiController {
         return orderQueryRepository.findOrderQueryDtos();
     }
 
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> orderV5() {
+        return  orderQueryRepository.findAllByDtoOptimization();
+    }
+
     @Data
     static class OrderDto {
         private Long orderId;
